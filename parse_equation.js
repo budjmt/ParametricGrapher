1c5f3b0bc2fa31d758f330c9d4c7b98054d91f19
Michael Cohen <budjmt@aol.com>
2016-04-15 03:17:45 -0400

    fix parser for all but functions, get graphing to basic functionality
    need implicit multiply before/after expression, not just number

--- parse_equation.js
+++ parse_equation.js
@@ -81,39 +81,50 @@ NOTES AND EDGE CASES:
 
 var errString = '';//used for error handling
 
+var orderOfOperations = {
+	NONE : -1,
+	FN : 0,
+	EXP : 1,
+	MUL : 2,
+	ADD : 3
+};
+Object.seal(orderOfOperations);
+Function.prototype.oo = orderOfOperations.NONE;
+
 var phi = (1 + Math.sqrt(5)) / 2;// 1.618033988749895
 var pi = Math.PI;
 var e = Math.E;
 
-var number = '\d+(\.\d+)?';
+var number = '\\d+(\\.\\d+)?';
 var constant_list = ['e','pi','phi'];
 var constant_vals = [ e, pi, phi ];
 var constant = constant_list.join('|');
 constant_list.forEach(function(element,index) { constant_list[index] = [ new RegExp(element), constant_vals[index] ]; });
 var number_regex_str = '(' + number + '|' + constant + ')';
 
-var log_regex_str = '(log(_' + number_regex_str + ')?|ln)';//if no base is included, it's assumed to be 10
+var log_regex_str = '(log_?|ln)';//if no base is included, it's assumed to be 10
 var trig_regex_str = '((a|arc)?((cos)|(sin)|(tan)|(sec)|(csc)|(cot))h?)';
 
-var add_regex = '(\+|-)';
-var mul_regex = '(\*|\/)';
-var operator_regex = '(\^|' + mul_regex + '|' + add_regex + ')';
+var add_regex = '(\\+|-)';
+var mul_regex = '(\\*|\\/)';
+var operator_regex_str = '(\\^|' + mul_regex + '|' + add_regex + ')';
 
 var finder_regex = new RegExp(
-'(^' + number_regex_str + '|^\(|^\)|^' + trig_regex_str + '|^' + log_regex_str + '|^(sqrt)' + '|^' + operator_regex +  '|^t)'
+'(^' + number_regex_str + '|^\\(|^\\)|^' + trig_regex_str + '|^' + log_regex_str + '|^(sqrt)' + '|^' + operator_regex_str + '|^t)'
 );
 
-var number_regex = new RegExp(number_regex_str);
-var trig_regex   = new RegExp(trig_regex_str);
-var log_regex    = new RegExp(log_regex_str);
+var number_regex   = new RegExp(number_regex_str);
+var trig_regex     = new RegExp(trig_regex_str);
+var log_regex      = new RegExp(log_regex_str);
+var operator_regex = new RegExp(operator_regex_str);
 
-function add(a,b) { return a + b; }
-function sub(a,b) { return a - b; }
-function mul(a,b) { return a * b; }
-function div(a,b) { return a / b; }
+function add(a,b) { return a + b; } add.oo = orderOfOperations.ADD;
+function sub(a,b) { return a - b; } sub.oo = orderOfOperations.ADD;
+function mul(a,b) { return a * b; } mul.oo = orderOfOperations.MUL;
+function div(a,b) { return a / b; } div.oo = orderOfOperations.MUL;
 
-function exp(a,b) { return Math.pow(a,b); } 
-function sqrt(a)  { return Math.sqrt(a);  }
+function exp(a,b) { return Math.pow(a,b); } exp.oo = orderOfOperations.EXP;
+function sqrt(a)  { return Math.sqrt(a);  } orderOfOperations.FN;
 
 function sin(a)  { return Math.sin(a);  }     function asin(a)  { return Math.asin(a);    }
 function sinh(a) { return Math.sinh(a); }     function asinh(a) { return Math.asinh(a);   }
@@ -125,8 +136,8 @@ function csc(a)  { return 1 / Math.sin(a);  } function acsc(a)  { return Math.as
 function csch(a) { return 1 / Math.sinh(a); } function acsch(a) { return Math.asinh(1/a); }
 function sec(a)  { return 1 / Math.cos(a);  } function asec(a)  { return Math.acos(1/a);  }
 function sech(a) { return 1 / Math.cosh(a); } function asech(a) { return Math.acosh(1/a); }
-function cot(a)  { return 1 / Math.tan(a);  } function cot(a)   { return Math.atan2(1/a); }
-function coth(a) { return 1 / Math.tanh(a); } function coth(a)  { return Math.atanh(1/a); }
+function cot(a)  { return 1 / Math.tan(a);  } function acot(a)  { return Math.atan2(1/a); }
+function coth(a) { return 1 / Math.tanh(a); } function acoth(a) { return Math.atanh(1/a); }
 var trig_fns = { 
 	sin:sin,asin:asin,arcsin:asin,sinh:sinh,asinh:asinh,arcsinh:asinh,
 	cos:cos,acos:acos,arccos:acos,cosh:cosh,acosh:acosh,arccosh:acosh,
@@ -135,62 +146,94 @@ var trig_fns = {
 	sec:sec,asec:asec,arcsec:asec,sech:sech,asech:asech,arcsech:asech,
 	cot:cot,acot:acot,arccot:acot,coth:coth,acoth:acoth,arccoth:acoth
  };
+ for(var fn in trig_fns) { trig_fns[fn].oo = orderOfOperations.FN; }
 
-function log(base,val) { return Math.log(val) / Math.log(base); }
-function ln(a) { return Math.log(a); }
+function log(base,val) { return Math.log(val) / Math.log(base); } log.oo = orderOfOperations.FN;
+function ln(a) { return Math.log(a); } ln.oo = orderOfOperations.FN;
 
-function abs(a) { return Math.abs(a); }
-
-var orderOfOperations = [ 'fn','exp','mul','add' ];
+function abs(a) { return Math.abs(a); } abs.oo = orderOfOperations.FN;
 
 var Expression = function(op,a,b) {
 	this.operation = op;
 	this.left = a;
 	this.right = b;
 };
-
+Expression.prototype.name = 'Expression';
+Expression.prototype.toString = function() { 
+	return '(' + this.operation.name + ' '
+	+ ((this.operation.length > 1) ? this.left.toString() + ', ' : '')
+	+ this.right.toString() + ')';
+}
 Expression.prototype.evaluate = function(t) {
-	var l = (this.left)  ? this.left.evaluate(t)  : this.left;//left should never be undefined, but w/e
-	var r = (this.right) ? this.right.evaluate(t) : this.right;
-	return this.operation(l, r);
+	return this.operation(this.left.evaluate(t), this.right.evaluate(t));
 }
 
 var T = function() { Expression.call(this); }
 T.prototype = Object.create(Expression.prototype);
+T.prototype.toString = function() { return this.name; }
+T.prototype.name = 'T';
 T.prototype.evaluate = function(t) { return t; }
 
 var Constant = function(val) { Expression.call(this); this.value = val; }
 Constant.prototype = Object.create(Expression.prototype);
+Constant.prototype.toString = function() { return this.value; }
+Constant.prototype.name = 'Constant';
 Constant.prototype.evaluate = function() { return this.value; }
 
 function genExpression(op,left,right) {
-	if(((left && left.value) || !left) && ((right && right.value) || !right))
-		return new Constant(op(left,right));
+	console.log(((op) ? op.name : op) + ', ' 
+	+ (left ? left.toString() : left) + ', ' 
+	+ (right ? right.toString() : right));
+	
+	if(!left)  left  = new Constant(0);
+	if(!right) right = new Constant(0);
+	if(left.value && right.value)
+		return new Constant(op(left.value,right.value));
+	if(op.length == 1 && left.value == 0 && !right.value) {
+		left = right;
+		right = new Constant(0);
+	}
 	return new Expression(op,left,right);
 }
 
 function getExpression(expressions,operations) {
+	console.log('Getting expression');
 	var opEnd = operations.length - 1;
 	var exprEnd = expressions.length - 1;
 	if(opEnd < 0) {
 		if(exprEnd < 0) return null;
-		else return expressions[exprEnd];//this is a valid constant or t
+		else return expressions.pop();//this is a valid constant or t
 	}
 	do {
-		var op = operations.pop(), left, right;
+		var op = operations.pop(), left = undefined, right = undefined;
 		--opEnd;
 		for(var i = 0, args = op.length; i < args; ++i) {
 			if(exprEnd < 0) {
 				errString = "Not enough arguments for function: " + operations[opEnd];
 				return undefined;
 			}
-			if(right) left  = expressions[exprEnd];
-			else                 right = expressions[exprEnd];
+			var expr = expressions.pop();
+			if(right) left  = expr;
+			else      right = expr;
 			--exprEnd;
 		}
 		expressions.push(genExpression(op,left,right));
-	} while(opEnd > 0);
-	return expressions[0];
+		exprEnd++;
+	} while(opEnd >= 0);
+	return expressions.pop();
+}
+
+function printStack(stack,depth) {
+	var str = '';
+	for(var i = 0; i < depth + 1;i++) {
+		str += '[';
+		for(var thing in stack[i]) {
+			var curr = stack[i][thing];
+			str += (curr.hasOwnProperty('left') ? curr.toString() : curr.name) + ', ';
+		}
+		str += '], ';
+	}
+	console.log(str);
 }
 
 function parseEquation(eqString) {
@@ -201,8 +244,6 @@ function parseEquation(eqString) {
 		errString = 'You must input an equation';
 		return undefined;
 	}
-	//format for regex
-	eqString = eqString.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 	var expressionList = [[]];
 	var operationList = [[]];
 	var depth = 0;
@@ -211,33 +252,16 @@ function parseEquation(eqString) {
 	while(eqString.length) {
 		//first expression present
 		//it matches each time because some results are dependent on others being processed
-		var expr = eqString.match(finder_regex)[0];
-		eqString = eqString.replace(finder_regex,'');
-		var currOp, num_present;
-		if(!expr.length) {
-			//the string has been depleted? Somehow?
-			errString = 'Ran out of characters to check';
+		var expr = eqString.match(finder_regex);
+		if(!expr) {
+			errString = 'Invalid equation';
 			return undefined;
 		}
-		else if(num_present = expr.test(number_regex) || expr.test(/t/)) {
-			var val;
-			if (expr.test(/\d+(\.\d+)?/)) { val = new Constant(Number.parseFloat(expr)); }
-			else if(num_present) {
-				constant_list.some(function(element,index) {
-					var found = expr.test(element[0]);
-					return found && val = new Constant(element[1]);
-				});
-			}
-			else { val = new T(); }
-			expressionList[depth].push(val);
-			//this needs to be generalized for ALL non-operations
-			if(expressionList[depth].length > 1 && numExprReq[depth] < expressionList[depth].length
-			&& !(lastOp[depth] == 'exp' || lastOp[depth] == 'mul' || lastOp[depth] == 'add')) {
-				//there's an implicit multiply
-				operations.push(mul);
-			}
-		}
-		else if(expr[0] == '(' || expr[0] == '|') {
+		expr = expr[0];
+		eqString = eqString.replace(finder_regex,'');
+		console.log('=> ' + expr + ', ' + eqString);
+		var currOp = undefined, num_present = false;
+		if(expr[0] == '(' || expr[0] == '|') {
 			//if we're processing an abs, to close:
 			//one of the previous levels' last op must also be an abs
 			//and the number of required expressions in the current level must be satisfied
@@ -259,7 +283,7 @@ function parseEquation(eqString) {
 			}
 		}
 		else if(expr[0] == ')') {
-			if(!depth || lastOp.some(function(el) { return el == '('; })) {
+			if(!depth || !lastOp.some(function(el) { return el == '('; })) {
 				errString = 'Closing ) found with no matching open (';
 				return undefined;
 			}
@@ -268,83 +292,132 @@ function parseEquation(eqString) {
 			depth--;
 			if(parenExpr != null) expressionList[depth].push(parenExpr);
 		}
-		else if(expr.test(trig_regex)) {
+		else if(trig_regex.test(expr)) {
 			operationList[depth].push(trig_fns[expr]);
 			currOp = 'fn';
-			numExprReq[depth] += trig_fns[expr].length;
+			numExprReq[depth] = (trig_fns[expr].length > numExprReq[depth]) ? 
+				trig_fns[expr].length : numExprReq[depth] + 1;
+			
 		}
-		else if(expr.test(log_regex)) {
+		else if(log_regex.test(expr)) {
 			if(expr[1] == 'n') {
 				operationList[depth].push(ln);
-				numExprReq[depth] += ln.length;
+				numExprReq[depth] = (ln.length > numExprReq[depth]) ? 
+					ln.length : numExprReq[depth] + 1;
 			}
 			else {
 				operationList[depth].push(log);
-				var base = expr.match(number_regex)[0];
-				if(base) expressionList[depth].push(new Constant(Number.parseFloat(base)));
-				else 	 expressionList[depth].push(new Constant(10));
-				numExprReq[depth] += log.length;
+				if(expr.length < 4)//there's no base specified
+					expressionList[depth].push(new Constant(10));
+				numExprReq[depth] = (log.length > numExprReq[depth]) ? 
+					log.length : numExprReq[depth] + 1;
 			}
 			currOp = 'fn';
 		}
-		else if(expr.text(/(sqrt)/) {
+		else if(/(sqrt)/.test(expr)) {
 			operationList[depth].push(sqrt);
 			currOp = 'fn';
-			numExprReq[depth] += sqrt.length;
+			numExprReq[depth] = (sqrt.length > numExprReq[depth]) ? 
+					sqrt.length : numExprReq[depth] + 1;
 		}
-		else if(expr.test(operator_regex)) {
-			if((expr[0] == '+' || expr[0] == '-') && numExprReq[depth] > expressions[depth].length) {
-				expressions[depth].push(new Constant(0));
+		else if(operator_regex.test(expr)) {
+			if((expr[0] == '+' || expr[0] == '-') 
+				&& numExprReq[depth] > expressionList[depth].length) {
+				expressionList[depth].push(new Constant(0));
 			}
 			if(expr[0] == '^' || expr[0] == 's') {
 				var trig_op;
-				else if( lastOp[depth] && trig_op = operations[depth][operations[depth].length - 1]
+				if( lastOp[depth] == 'fn'
+					&& (trig_op = operationList[depth][operationList[depth].length - 1])
 					&& trig_fns.hasOwnProperty(trig_op.name) ) {
 					//special case for trig exponents; we have to swap the operations
 					//because the exponent would come second
+					//unfortunately, the way this works for trig^e(n) we end up with 
+					//(exp e (trig n)) instead of (exp (trig n) e)
+					//so this is broken right now
 					operationList[depth].push(trig_op);
 					operationList[depth][operationList[depth].length - 2] = exp;
 					currOp = 'fn';
-					lastOp[depth] = 'exp';//for the swap to work
+					lastOp[depth] = 'op';//for the swap to work
 				}
 				else {
 					operationList[depth].push(exp);
-					currOp = 'exp';
+					currOp = 'op';
 				}
-				numExprReq[depth] += exp.length;
+				numExprReq[depth] = (exp.length > numExprReq[depth]) ? 
+					exp.length : numExprReq[depth] + 1;
 			}
 			else if(expr[0] == '*') {
 				operationList[depth].push(mul);
-				currOp = 'mul';
-				numExprReq[depth] += mul.length;
+				currOp = 'op';
+				numExprReq[depth] = (mul.length > numExprReq[depth]) ? 
+					mul.length : numExprReq[depth] + 1;
 			}
 			else if(expr[0] == '/') {
 				operationList[depth].push(div);
-				currOp = 'mul';
-				numExprReq[depth] += div.length;
+				currOp = 'op';
+				numExprReq[depth] = (div.length > numExprReq[depth]) ? 
+					div.length : numExprReqp[depth] + 1;
 			}
 			else if(expr[0] == '+') {
 				operationList[depth].push(add);
-				currOp = 'add';
-				numExprReq[depth] += add.length;
+				currOp = 'op';
+				numExprReq[depth] = (add.length > numExprReq[depth]) ? 
+					add.length : numExprReq[depth] + 1;
 			}
 			else if(expr[0] == '-') {
 				operationList[depth].push(sub);
-				currOp = 'add';
-				numExprReq[depth] += sub.length;
+				currOp = 'op';
+				numExprReq[depth] = (sub.length > numExprReq[depth]) ? 
+					sub.length : numExprReq[depth] + 1;
 			}
-			
-			if( currOp && lastOp[depth] 
-			&& orderOfOperations.indexOf(currOp) < orderOfOperations.indexOf(lastOp[depth]) ) {
-				//we must evaluate an expression since we broke order of operations
-				var op = operationList.pop();
-				expressionList[depth] = 
-					[ getExpression( expressionList[depth], operationList[depth] ) ];
-				operationList[depth] = [ op ];
-				numExprReq[depth] = op.length;
-			}
-			lastOp[depth] = currOp;
 		}
+		else if((num_present = number_regex.test(expr)) || /t/.test(expr)) {
+			var val;
+			if (/\d+(\.\d+)?/.test(expr)) { val = new Constant(Number.parseFloat(expr)); }
+			else if(num_present) {
+				constant_list.some(function(element,index) {
+					return element[0].test(expr) && (val = new Constant(element[1]));
+				});
+			}
+			else { val = new T(); }
+			currOp = 'num';
+			var before = numExprReq[depth] < expressionList[depth].length;
+			var after;
+			expressionList[depth].push(val);
+			//this needs to be generalized for ALL non-operations
+			if(expressionList[depth].length > 1 
+			&& (after = numExprReq[depth] < expressionList[depth].length)
+			&& lastOp[depth] != 'op') {
+				//there's an implicit multiply
+				operationList[depth].push(mul);
+				if(before == after) currOp = 'op';
+				else                lastOp[depth] = 'op';
+				numExprReq[depth] = (mul.length > numExprReq[depth]) ? 
+					mul.length : numExprReq[depth] + 1;
+				//if we break order of operations, we need to step back
+				var numOps = operationList[depth].length;
+				if(after && numOps > 1 && operationList[depth][numOps - 2].oo < operationList[depth][numOps - 1].oo) {
+					expressionList[depth].pop();
+					eqString = expr + eqString;
+				}
+			}
+		}
+		
+		//console.log(lastOp[depth] + ', ' + currOp);
+		var numOps = operationList[depth].length;
+		if( numOps > 1 &&
+			operationList[depth][numOps - 2].oo < operationList[depth][numOps - 1].oo ) {
+			//we must evaluate an expression since we broke order of operations
+			var op = operationList[depth].pop();
+			expressionList[depth] = 
+				[ getExpression( expressionList[depth], operationList[depth] ) ];
+			operationList[depth] = [ op ];
+			numExprReq[depth] = op.length;
+		}
+		lastOp[depth] = currOp;
+		printStack(expressionList,depth);
+		printStack(operationList,depth);
 	}
 	if(depth > 0) {
 		errString = 'Open ' + lastOp[depth - 1] + ' found with no closure';
